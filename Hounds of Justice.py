10d71b1765a665430524a128e11fa803a778c8cb
jhuntley97 <jhuntley@my.nl.edu>
2019-03-21 15:21:55 -0500

    Add files via upload

---
+++ Hounds of Justice.py
@@ -0,0 +1,82 @@
+#Jwaun Huntley
+#2/28/19
+
+
+def Introduction():
+#User will have a flashback
+    print("Wild West")
+    print("User character father makes his mother cry")
+    print("User interact with his father")
+    print("Father responds back to the user")
+    print("press 1 to defend his mother")
+    print("press 2 to call the outlaws")
+
+    x = input("press 1 or 2 -")
+    if x=='1':
+        print("user father dies and now his mother is terrified")
+
+    if x=='2':
+        print("user calls outlaws but outlaws are too late user mother dies")
+
+
+Introduction()
+print("User character is talking to his friend")
+print("User character and friend want to rob a bank")
+print("User friend gave him a scarf to cover his face then kicked in the door then ask for the money")
+print("User friend aims gun the civilian")
+print("User character goes to the vault to get the money")
+print("User friend is being held hostage")
+print("User character save his friend from almost being killed")
+print("press 1 to get the money")
+print("press 2 to kill the civilan")
+
+x = input("press 1 or 2 -")
+if x=='1':
+    print("User character gets away peacefully but User friend is wanted")
+
+if x=='2':
+    print("User character get away but is wanted and friend got the money")
+
+    
+print("User character is exploring the Old West Texas with his friend")
+print("User character sees an abandoned house")
+print("User charcter talked his friend about being honorable")
+print("People were in the abandoned house")
+print("User character have to kill all the people to take over the abandoned house")
+print("press 2 to shoot")
+for r_variable in range(10):
+    x = input("press 2 to shoot")
+    if x != '2':
+        print("miss target")
+
+print("User character meets someone new")
+print("User character is interacting with person that don't have nowhere to stay")
+print("User character offer the person somewhere to stay")
+print("press 1 to get on horse")
+print("User character takes the person to his abandoned home")
+
+x = input("press 1")
+while x !='1':
+    x = input("press 1")
+
+print("User character becomes a bounty hunter to make some more money")
+print("User character is hunting for the bounty hunter saw the person he was looking for")
+print("User character almost got killed person refused to turn himself in")
+print("press 1 to kill the person")
+print("User character go back to the sheriff to collect the money")
+
+
+x = input("press 1")
+while x !='1':
+    x = input("press 1")
+print("User character kills the person but gets less money and get killed leaving the station")
+
+
+
+
+    
+      
+
+    
+
+    
